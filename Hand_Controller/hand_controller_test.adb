e18db4b129f700640abca5866fa27dbada457c60
justiceadamsUNI <adamsjal@uni.edu>
2017-11-20 20:36:13 -0600

    adressed PR comments

--- Hand_Controller/hand_controller_test.adb
+++ Hand_Controller/hand_controller_test.adb
@@ -13,29 +13,16 @@ procedure Hand_Controller_Test is
    Black_Button : Hand_Controllers.Button_State_Type;
    Direction_Switch: Hand_Controllers.Direction_Switch_Type;
    Turn_Switch: Hand_Controllers.Turn_Switch;
-   Throttle_Level: Hand_Controllers.Throttle_Level;
-   Controller: Hand_Controllers.Controller_ID;
+   Controller_Throttle: Hand_Controllers.Throttle_Level;
 begin
    loop
-      Ada.Text_IO.Put_Line ("Press Enter to display all three hand controllers data");
+      Ada.Text_IO.Put_Line  (Item => "Press Enter to display all three hand controllers data");
       Ada.Text_IO.Skip_Line;
 
       -- Loop through all 3 hand controllers
-      for I in Integer range 1...3 loop
-         if I = 1 then
-            Controller = Hand_Controllers.A;
-            Ada.Text_IO.Put_Line("---------- Controller A ----------");
-         end if;
+      for Controller in Hand_Controllers.Controller_ID loop
+         Ada.Text_IO.Put_Line (Item => "---------- Controller " & Hand_Controllers.Controller_ID'Image(Controller)  & " ----------");
 
-         if I = 2 then
-            Controller = Hand_Controllers.B;
-            Ada.Text_IO.Put_Line("---------- Controller B ----------");
-         end if;
-
-         if I = 3 then
-            Controller = Hand_Controllers.C;
-            Ada.Text_IO.Put_Line("---------- Controller C ----------");
-         end if;
 
          -- Get data for controller
          Hand_Controllers.Get_Digital_State(ID              => Controller,
@@ -44,44 +31,21 @@ begin
                                             dirction_switch => Direction_Switch,
                                             turn_switch     => Turn_Switch);
 
-         Hand_Controllers.Get_Analog_State(ID                 => Controller,
-                                           Controller_Voltage => Throttle_Level);
+         Hand_Controllers.Get_Analog_State(ID       => Controller,
+                                           Throttle => Controller_Throttle);
 
-         Ada.Text_IO.Put("Red button state: ");
-         if Red_Button = Hand_Controllers.Up then
-            Ada.Text_IO.Put_Line("Up");
-         else
-            Ada.Text_IO.Put_Line("Down");
-         end if;
+         -- Print out states
+         Ada.Text_IO.Put_Line (Item => "Red button state: " & Hand_Controllers.Button_State_Type'Image (Red_Button));
 
-         Ada.Text_IO.Put("Black button state: ");
-         if Black_Button = Hand_Controllers.Up then
-            Ada.Text_IO.Put_Line("Up");
-         else
-            Ada.Text_IO.Put_Line("Down");
-         end if;
+         Ada.Text_IO.Put_Line (Item => "Black button state: " & Hand_Controllers.Button_State_Type'Image (Black_Button));
 
-         Ada.Text_IO.Put("Direction switch state: ");
-         if Direction_Switch = Hand_Controllers.Backward then
-            Ada.Text_IO.Put_Line("Backward");
-         else
-            Ada.Text_IO.Put_Line("Forward");
-         end if;
+         Ada.Text_IO.Put_Line (Item => "Direction switch state: " & Hand_Controllers.Direction_Switch_Type'Image (Direction_Switch));
 
-         Ada.Text_IO.Put("Turn switch state: ");
-         if Direction_Switch = Hand_Controllers.Left then
-            Ada.Text_IO.Put_Line("Left");
-         else
-            if Direction_Switch = Hand_Controllers.Right then
-               Ada.Text_IO.Put_Line("Right");
-            else
-               Ada.Text_IO.Put_Line("Centered");
-            end if;
-         end if;
+         Ada.Text_IO.Put_Line (Item => "Turn switch state: " &  Hand_Controllers.Turn_Switch'Image (Turn_Switch));
 
-         Ada.Text_IO.Put("Throttle Level: ");
-         Ada.Text_IO.Put_Line(Integer'Image(Controller_Voltage));
-         Ada.Text_IO.Put_Line("----------------------------------");
+         Ada.Text_IO.Put_Line (Item => "Throttle Level: " & Hand_Controllers.Throttle_Level'Image (Controller_Throttle));
+
+         Ada.Text_IO.Put_Line (Item => "----------------------------------");
       end loop;
 
    end loop;
