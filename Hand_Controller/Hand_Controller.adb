892a6c2b52c8d8018c677df6fc69343a01fad966
Daniel Alonso <alonsod@uni.edu>
2017-12-07 12:08:31 -0600

    Fixed other issues based on professor Bern's previous comments.

--- Hand_Controller/Hand_Controller.adb
+++ Hand_Controller/Hand_Controller.adb
@@ -7,7 +7,7 @@ package body Hand_Controller is
             Red : Button_State_Type;
             Black : Button_State_Type;
             Direct_Switch : Direction_Switch_Type;
-            Turn_Switch : Direction_Switch_Type;
+            Turn_Switch : Turn_Switch_Type;
         end record; 
 
     for HC_Record use
@@ -18,31 +18,21 @@ package body Hand_Controller is
             Turn_Switch at 3 range 3 .. 4;
         end record;
 
-    for HC_Record'Size use 8;
-
-    function Byte_To_HC_Record is new Ada.Unchecked_Conversion(Source => Port_IO.Byte,
-            Target => HC_Record);
+    for HC_Record use
+        function
+            function Byte_To_HC_Record is new Ada.Unchecked_Conversion(Source => Port_IO.In_Byte,
+                    Target => HC_Record);
+        end function;
 
     procedure Get_Digital_State(ID : in Controller_ID;
                                 Red_Button : out Button_State_Type;
                                 Black_Button : out Button_State_Type;
                                 Dirction_Switch : out Direction_Switch_Type;
-                                Turn_Switch2 : out Direction_Switch_Type) is
-    Parameters_In : Port_IO.In_Byte;
+                                Turn_Switch : out Turn_Switch_Type) is
+    Parameters_In : Controller_ID.ID;
     New_Num : HC_Record;
     begin 
-        Parameters_In.Red := Red_Button;
-        Parameters_In.Black := Black_Button;
-        Parameters_In.Direct_Switch := Dirction_Switch;
-        Parameters_In.Turn_Switch := Turn_Switch2;
-
         New_Num := Byte_To_HC_Record(Parameters_In);
-        
-        Red_Button := Parameters_In.Red;
-        Black_Button := Parameters_In.Black
-        Dirction_Switch := Parameters_In.Direct_Switch;
-        Turn_Switch2 := Parameters_In.Turn_Switch;
-        
     end Get_Digital_State;
 
 
