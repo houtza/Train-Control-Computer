9302a3f2f5427f930ae10662ce835fa66520dc52
justiceadamsUNI <adamsjal@uni.edu>
2017-12-07 19:37:29 -0600

    dun goofed

--- Hand_Controller/hand_controllers.adb
+++ Hand_Controller/hand_controllers.adb
@@ -2,12 +2,9 @@ with Port_IO;
 with Ada.Unchecked_Conversion;
 with Analog_Digital_Converter;
 	
-
-	
 package body Hand_Controller.Hand_Controllers is
-   
    procedure Get_Analog_State(ID : in Controller_ID; Controller_Voltage : out Throttle_Level) is
-    begin
-        Controller_Voltage := Analog_Digital_Converter.Convert(;
-    end Get_Analog_State;				 
+   begin
+      Controller_Voltage := Analog_Digital_Converter.Convert(Channel_Type(ID), Controller_Voltage) * 20;
+   end Get_Analog_State;				 
 end Hand_Controllers;
