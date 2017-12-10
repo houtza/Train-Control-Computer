85c9002b0dca9884f311b169118f0edbebf16354
Justice Adams <adamsjal@uni.edu>
2017-12-10 11:57:54 -0600

    Update hand_controllers.adb

--- Hand_Controller/hand_controllers.adb
+++ Hand_Controller/hand_controllers.adb
@@ -2,9 +2,11 @@ with Port_IO;
 with Ada.Unchecked_Conversion;
 with Analog_Digital_Converter;
 	
-package body Hand_Controller.Hand_Controllers is
+package body Hand_Controllers is
    procedure Get_Analog_State(ID : in Controller_ID; Controller_Voltage : out Throttle_Level) is
+      Voltage : Analog_Digital_Converter.Voltage_Range;
    begin
-      Controller_Voltage := Analog_Digital_Converter.Convert(Channel_Type(ID), Controller_Voltage) * 20;
+      Analog_Digital_Converter.Convert(Channel_Type (Controller_ID'Pos (ID)), Voltage);
+      Controller_Voltage := Throttle_Level (Float(Voltage) * 20.0);
    end Get_Analog_State;				 
 end Hand_Controllers;
