8a23a350ad7b9e80ec656ad50f7bca6f340ae6dd
Aaron Houtz <houtza@uni.edu>
2017-12-11 15:10:07 -0600

    Update dac.adb and made correction to code

--- DAC/dac.adb
+++ DAC/dac.adb
@@ -8,8 +8,8 @@ package body DAC is
       word : Port_IO.Word;
       
    begin 
-      
-      word := Port_IO( Float(Value)*409.5);
+              
+      word :=Port_IO.Word ((Float(Value) + 5.0) * 409.5)
       Port_IO.Out_Word(Address=>16#240#+ (Integer(Channel)*2),Data=>word);
       
    end Set;
